4ce4a2934e22941c1ee34ce197ba803dbf67617f
heptaliane <heptaliane@gmail.com>
2018-03-28 02:25:17 +0900

    Auto-generated commit

--- handler/TwitterHandler.js
+++ handler/TwitterHandler.js
@@ -1,7 +1,7 @@
-import tweet from 'twitter';
+import Twitter from 'twitter';
 import config from '../config/twitter.json';
 
-const client = new twitter({
+const client = new Twitter({
   consumer_key: config.consumer_key,
   consumer_secret: config.consumer_secret,
   access_token_key: config.access_token_key,
@@ -11,14 +11,14 @@ const client = new twitter({
 export default function(url) {
   return new Promise((resolve, reject) => {
     client.post('statuses/update', {status: `${config.content}\n ${url}`},
-      (err) => {
+      (err, tweet) => {
         // error occur
         if (err === null) {
-          reject();
+          reject(err);
 
         // success
         } else {
-          resolve();
+          resolve(tweet);
         }
       });
   });
